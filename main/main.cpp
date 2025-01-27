69eb45ccea3d259c20e13a1e1edb53757b9091c0
Caleb Snow <caleb.snow@circuitboardmedics.com>
2025-01-27 13:03:10 -0500

    Formatting and variable name changes

--- main/main.cpp
+++ main/main.cpp
@@ -9,16 +9,16 @@
 
 constinit cbm::MCP2518 mcp;
 
-constexpr gpio_num_t cPrimarySpiCsPin = static_cast<gpio_num_t>(CONFIG_SPI_CS_PIN);
-constexpr gpio_num_t cSpiMosiPin      = static_cast<gpio_num_t>(CONFIG_SPI_MOSI_PIN);
-constexpr gpio_num_t cSpiMisoPin      = static_cast<gpio_num_t>(CONFIG_SPI_MISO_PIN);
-constexpr gpio_num_t cSpiClkPin       = static_cast<gpio_num_t>(CONFIG_SPI_CLK_PIN);
-constexpr gpio_num_t cRxIntrPin       = static_cast<gpio_num_t>(CONFIG_RX_INTR_PIN);
-constexpr gpio_num_t cTxIntrPin       = static_cast<gpio_num_t>(CONFIG_TX_INTR_PIN);
+constexpr gpio_num_t cSpiCsPin   = static_cast<gpio_num_t>(CONFIG_SPI_CS_PIN);
+constexpr gpio_num_t cSpiMosiPin = static_cast<gpio_num_t>(CONFIG_SPI_MOSI_PIN);
+constexpr gpio_num_t cSpiMisoPin = static_cast<gpio_num_t>(CONFIG_SPI_MISO_PIN);
+constexpr gpio_num_t cSpiClkPin  = static_cast<gpio_num_t>(CONFIG_SPI_CLK_PIN);
+constexpr gpio_num_t cRxIntrPin  = static_cast<gpio_num_t>(CONFIG_RX_INTR_PIN);
+constexpr gpio_num_t cTxIntrPin  = static_cast<gpio_num_t>(CONFIG_TX_INTR_PIN);
 #ifdef CONFIG_SPI2_ENABLE
-constexpr spi_host_device_t cSpiHost  = SPI2_HOST;
+constexpr spi_host_device_t cSpiHost = SPI2_HOST;
 #elif defined(CONFIG_SPI3_ENABLE)
-constexpr spi_host_device_t cSpiHost  = SPI3_HOST;
+constexpr spi_host_device_t cSpiHost = SPI3_HOST;
 #endif
 
 static const char* TAG = "MCP2518FD Example";
@@ -46,15 +46,15 @@ extern "C" void app_main(void)
 
     cbm::MCP2518::Config cfg = {
         .spi_host    = cSpiHost,
-        .cs_pin      = cPrimarySpiCsPin,
+        .cs_pin      = cSpiCsPin,
         .tx_intr_pin = cTxIntrPin,
         .rx_intr_pin = cRxIntrPin,
     };
 
     mcp.begin(cfg);
     cbm::can_frame_t frame;
+    uint32_t ts;
     for (;;) {
-        uint32_t ts;
         if (mcp.ReadMessage(frame, ts) != cbm::MCP2518::Status::NO_MSG) {
             mcp.SendMessage(frame);
             ESP_LOGI(TAG, "Frame received at %luus", ts);
